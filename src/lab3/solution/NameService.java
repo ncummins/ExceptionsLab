cdb74ab2d8c59b982d5774056a2bbbd81f5b387c
ncummins <ncummins@L11106038622.wctc.msft>
2012-11-15 13:45:58 -0600

    asdf

---
+++ src/lab3/solution/NameService.java
@@ -0,0 +1,63 @@
+package lab3.solution;
+
+import lab3.*;
+
+/**
+ * This class provides various services relating to name manipulation.
+ * No output should be performed here.
+ * 
+ * @author  Jim Lombardo, jlombardo@wctc.edu
+ * @version 1.00
+ */
+public class NameService {
+    private static final int FIRST_NAME_IDX = 0;
+    private static final int LAST_NAME_IDX = 1;
+    
+    /**
+     * Finds and returns the last name from within a full name. 
+     * 
+     * @param fullName - a name containing a first name and a last name
+     * @return the last name
+     */
+    public String extractLastName(String fullName) throws InvalidNameException{
+        //name validation
+        if (!fullName.contains(" ")) {
+            throw new InvalidNameException();
+        }
+        
+        String[] nameParts = fullName.split(" ");
+        return nameParts[LAST_NAME_IDX];
+    }
+    
+    /**
+     * Finds and returns the first name from within a full name. 
+     * 
+     * @param fullName - a name containing a first name and a last name
+     * @return the first name
+     */
+    public String extractFirstName(String fullName) throws InvalidNameException{
+        //name validation
+        if (!fullName.contains(" ")) {
+            throw new InvalidNameException();
+        }
+        
+        String[] nameParts = fullName.split(" ");
+        return nameParts[FIRST_NAME_IDX];
+    }
+
+    /**
+     * Gets the length of a name.
+     * 
+     * @param name - any full name or part of a name.
+     * @return the length of the name or part.
+     */
+    public int getNameLength(String name) throws InvalidNameException{
+        //name validation
+        if(name == null){
+            throw new InvalidNameException();
+        }
+        
+        return name.length();
+    }
+    
+}
